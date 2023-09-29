5405ae3a927d6db77f4d49c4f73097b252eb1dc7
Hideks <hideksys@gmail.com>
2023-09-29 04:50:21 -0300

    implement soft delete

---
+++ prisma/seed.ts
@@ -0,0 +1,42 @@
+import { PrismaClient, Prisma } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+const userData: Prisma.UserCreateInput[] = [
+  {
+    name: 'Alice',
+    email: 'alice@prisma.io',
+    password: 'strong-password'
+  },
+  {
+    name: 'Nilu',
+    email: 'nilu@prisma.io',
+    password: 'strong-password'
+  },
+  {
+    name: 'Mahmoud',
+    email: 'mahmoud@prisma.io',
+    password: 'strong-password'
+  }
+]
+
+async function main() {
+  console.log(`Start seeding ...`)
+  for (const u of userData) {
+    const user = await prisma.user.create({
+      data: u,
+    })
+    console.log(`Created user with id: ${user.id}`)
+  }
+  console.log(`Seeding finished.`)
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
